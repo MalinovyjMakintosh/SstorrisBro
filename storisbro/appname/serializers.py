7e19a2554fe8ca2fb65917c7cfc32dced5c0096d
MalinovyjMakintosh <140470195+MalinovyjMakintosh@users.noreply.github.com>
2023-11-08 23:44:35 +0300

    2

--- storisbro/appname/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import CustomUser
-
-class UserRegistrationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['email', 'password']
